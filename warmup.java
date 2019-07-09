6f4052533d577709eb8fb6d82c622bb96e4c5388
Dorrianj <52667212+Dorrianj@users.noreply.github.com>
2019-07-09 13:13:56 -0400

    Update warmup.java

--- warmup.java
+++ warmup.java
@@ -1,5 +1,28 @@
-package day2;
+package practice;
 
-public class warmup {
+import java.util.Scanner;
 
-}
+public class me {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+Scanner sncr= new Scanner(System.in);
+	
+		
+		System.out.println("did you go to colleg for coding");
+		sncr.next();//strings 
+		System.out.println("how old were you when you started doing coding");
+		sncr.nextInt();//numbers 
+		System.out.println("what is your favorite thing about coding");
+		sncr.next();//strings 
+		System.out.println("what made you want to do coding");
+		sncr.nextInt();//strings 
+		
+		System.out.println("thanks for your participation");
+		sncr.close();
+		
+		
+  }
+  
+  {
